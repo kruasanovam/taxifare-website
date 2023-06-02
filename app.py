5b71f26e98c493c15ee4b9287c566c13c7c3107c
Maria Rusanova-Jardan <kruasanovam@gmail.com>
2023-06-02 13:21:44 +0200

    small update

--- app.py
+++ app.py
@@ -52,8 +52,29 @@ if submit_button:
     resp = requests.get(url, params=data)
     prediction = resp.json()
     fare = "{:.2f}".format(prediction["fare_amount"])
-    st.write(f"YOUR IMAGINARY FAIR PRICE IS {fare}")
+    st.title("YOUR IMAGINARY FAIR PRICE IS")
+    st.write({fare})
+
+
+
+
     st.markdown(
         f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
         unsafe_allow_html=True
         )
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('taxi.jpg')
